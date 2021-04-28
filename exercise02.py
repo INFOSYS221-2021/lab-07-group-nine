96fc1338da3703cbe700e56331e10d541ba5daed
pbol970 <79953088+pbol970@users.noreply.github.com>
2021-04-28 17:02:22 +1200

    Update exercise02.py
    done

--- exercise02.py
+++ exercise02.py
@@ -13,12 +13,14 @@ print("Current time: ", datetime_object)
 
 # modify the following statement that creates another datetime_object.
 # the second datetime_object contains information about yesterday
-datetime_object2 = datetime.datetime.timedelta(days = 1)
+# datetime_object2 = datetime.datetime.timedelta(days = 1)
+today = datetime.datetime.now()
+yesterday = today - datetime.timedelta(days=1)
 
 # print yesterday's date and time
-print("Yesterday's date and time: ", datetime_object2)
+print("Yesterday's date and time: ", yesterday)
 
-# modify the following statement to print the current date and time in
+# modify the following statement to print the current date and time in 
 # dd-mm-YY H:M:S format
 datetime_object3 = datetime_object
-print("Current date and time: ", datetime_object3.strftime(%b, %d, &y, %I, %M, %S))
+print("Current date and time: ", datetime_object3.strftime("%d-%m-%y %I:%M:%S"))
